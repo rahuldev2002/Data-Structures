2f4a518db57956262acc51677157d8205a6a57b8
rahuldev2002 <141893950+rahuldev2002@users.noreply.github.com>
2023-08-10 09:18:01 +0530

    Create 2c hashing.c

---
+++ 2c hashing.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main(){
+int n,m,k,u,v;
+scanf("%d%d",&n,&m);
+int a[n],b[m],c[m];
+for(int i=0;i<n;i++)
+scanf("%d",&a[i]);
+for(int i=0;i<m;i++){
+c[i]=0;
+b[i]=0;
+}
+for(int i=0;i<n;i++){
+k=a[i]%m;
+if(c[k]==0){
+b[k]=a[i];
+c[k]=1;
+}
+else{
+for(int j=0;j<m;j++){
+u=k;
+v=1+(a[i]%(m-1));
+u=(u+j*v)%m;
+if(c[u]==0){
+b[u]=a[i];
+c[u]=1;
+break;
+}
+}
+}
+}
+for(int i=0;i<m;i++)
+printf("%d ",b[i]);
+printf("\n%d\n",count);
+}
